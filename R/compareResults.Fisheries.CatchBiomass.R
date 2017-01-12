6f16e0f9998386c88e4b7db5932daffdca84c4d8
wStockhausen <william.stockhausen@noaa.gov>
2017-01-11 19:11:57 -0800

    Changes to eliminate unnecessary output.

--- R/compareResults.Fisheries.CatchBiomass.R
+++ R/compareResults.Fisheries.CatchBiomass.R
@@ -67,7 +67,7 @@ compareResults.Fisheries.CatchBiomass<-function(objs,
     #----------------------------------
     plots<-list();
     uF<-unique(mdfr$fleet);
-    print(uF);
+    if (verbose) print(uF);
     if (sum(grep('z',cast,fixed=TRUE))>0){
         #plot size comps by year
         if (verbose) cat("Plotting size comps\n")
