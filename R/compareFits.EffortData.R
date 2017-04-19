32e1645441e691994ce24f9d654dc8124fc21683
wStockhausen <william.stockhausen@noaa.gov>
2017-04-19 09:56:41 -0700

    Added compareFits.EffortData and .GrowthData. Incremented version to 2017.04.19.

--- R/compareFits.EffortData.R
+++ R/compareFits.EffortData.R
@@ -1,7 +1,7 @@
 #'
-#'@title Compare fits to growth data among several model runs
+#'@title Compare fits to effort data among several model runs
 #'
-#'@description Function to compare fits to growth data among several model runs.
+#'@description Function to compare fits to effort data among several model runs.
 #'
 #'@param objs - list of resLst objects
 #'@param dodge - width to dodge overlapping series
@@ -17,7 +17,7 @@
 #'
 #'@export
 #'
-compareFits.GrowthData<-function(objs,
+compareFits.EffortData<-function(objs,
                                  dodge=0.2,
                                  pdf=NULL,
                                  showPlot=FALSE,
@@ -30,7 +30,7 @@ compareFits.GrowthData<-function(objs,
         on.exit(dev.off());
         showPlot<-TRUE;
     }
-
+    
     cases<-names(objs);
 
     mdfr<-NULL;
@@ -38,7 +38,7 @@ compareFits.GrowthData<-function(objs,
         obj<-objs[[case]];
         #if (inherits(obj,"tcsam2013.resLst")) mdfr1<-rTCSAM2013::getMDFR.Pop.MeanGrowth(obj,verbose);
         #if (inherits(obj,"rsimTCSAM.resLst")) mdfr1<-rsimTCSAM::getMDFR.Pop.MeanGrowth(obj,verbose);
-        if (inherits(obj,"tcsam02.resLst"))   mdfr1<-rTCSAM02::getMDFR.Fits.GrowthData(obj,verbose);
+        if (inherits(obj,"tcsam02.resLst"))   mdfr1<-rTCSAM02::getMDFR.Fits.EffortData(obj,verbose);
         if (!is.null(mdfr1)){
             mdfr1$case<-case;
             mdfr<-rbind(mdfr,mdfr1);
@@ -46,62 +46,62 @@ compareFits.GrowthData<-function(objs,
     }
     
     if (is.null(mdfr)) {
-        cat("\n \nNo fits to growth data.\n \n")
+        cat("\n \nNo fits to effort data.\n \n")
         return(NULL);
     }
     
-    mdfr$z<-as.numeric(mdfr$z)
     mdfr$case<-factor(mdfr$case,levels=cases);
-    mdfr$y<-as.character(mdfr$y);
-
+    
     pd<-position_dodge(width=dodge);
-    datasets<-unique(mdfr$category);
+    pcs<-unique(mdfr$pc);
     plots<-list();
-    for (d in datasets){
-        mdfrp<-mdfr[(mdfr$category==d),];
+    for (pc in pcs){
+        mdfrp<-mdfr[(mdfr$pc==pc),];
+        fsh<-unique(mdfrp$fleet);
         #-------------------------------------------#
-        #plot growth data and fits
+        #plot effort data and fits
         #-------------------------------------------#
         mdfrpo<-mdfrp[mdfrp$type == 'observed', ];
         mdfrpp<-mdfrp[mdfrp$type == 'predicted',];
-        p <- ggplot(mdfrp,aes_string(x='z',y='val',colour='case',shape='case'));
+        mdfrpo$y<-as.numeric(mdfrpo$y);
+        mdfrpp$y<-as.numeric(mdfrpp$y);
+        p <- ggplot(mdfrp,aes_string(x='y',y='val',colour='case'));
         p <- p + geom_point(data=mdfrpo,position=pd);
         p <- p + geom_line(data=mdfrpp,position=pd);
-        if (any(!is.na(mdfr$lci))) p <- p + geom_errorbar(aes_string(ymin='lci',ymax='uci'),position=pd);
-        p <- p + geom_abline(slope=1,linetype=2);
-        p <- p + labs(x='pre-molt size (mm CW)',y="post-molt size (mm CW)");
-        p <- p + ggtitle(d);
-        p <- p + facet_grid(x~.);
+        if (any(!is.na(mdfrpo$lci))) p <- p + geom_errorbar(data=mdfrpo,aes_string(ymin='lci',ymax='uci'),position=pd);
+        p <- p + labs(x='year',y="effort");
+        p <- p + ggtitle(fsh);
+        p <- p + facet_grid(x+m+s~.);
         if (showPlot) print(p);
-        cap<-paste0("\n  \nFigure &&figno. Model fits to ",d,".\n   \n")
-        plots[[cap]]<-p;
-        #-------------------------------------------#
-        #plot nll scores
-        #-------------------------------------------#
-        mdfrpn<-mdfrp[mdfrp$type == 'nlls',];
-        p <- ggplot(mdfrpn,aes_string(x='z',y='val',colour='case',shape='case'));
-        p <- p + geom_point(position=pd);
-        p <- p + geom_abline(slope=0,linetype=2);
-        p <- p + labs(x='pre-molt size (mm CW)',y="NLLs");
-        p <- p + ggtitle(d);
-        p <- p + facet_grid(x~.);
-        if (showPlot) print(p);
-        cap<-paste0("\n  \nFigure &&figno. Negative log-likelihood values for fits to ",d,".\n   \n")
+        cap<-paste0("\n  \nFigure &&figno. Model fits to effort in the ",fsh,".\n   \n")
         plots[[cap]]<-p;
         #-------------------------------------------#
         #plot zscores
         #-------------------------------------------#
         mdfrpz<-mdfrp[mdfrp$type == 'zscores',];
-        p <- ggplot(mdfrpz,aes_string(x='z',y='val',colour='case',shape='case'));
+        p <- ggplot(mdfrpz,aes_string(x='y',y='val',colour='case'));
         p <- p + geom_point(position=pd);
         p <- p + geom_abline(slope=0,linetype=2);
-        p <- p + labs(x='pre-molt size (mm CW)',y="z-scores");
-        p <- p + ggtitle(d);
-        p <- p + facet_grid(x~.);
+        p <- p + labs(x='year',y="z-scores");
+        p <- p + ggtitle(fsh);
+        p <- p + facet_grid(x+m+s~.);
         if (showPlot) print(p);
-        cap<-paste0("Figure &&figno. \n  \nZ-scores for fits to ",d,".\n   \n")
+        cap<-paste0("Figure &&figno. \n  \nZ-scores for fits to effort in the ",fsh,".\n   \n")
         plots[[cap]]<-p;
     }#d
 
+    #-------------------------------------------#
+    #plot nll scores
+    #-------------------------------------------#
+    mdfrpn<-mdfr[mdfr$type == 'nlls',];
+    p <- ggplot(mdfrpn,aes_string(x='fleet',y='val',fill='case'));
+    p <- p + geom_col(position="dodge");
+    p <- p + geom_abline(slope=0,linetype=2);
+    p <- p + labs(x='fishery',y="NLLs");
+    p <- p + facet_grid(x+m+s~.);
+    if (showPlot) print(p);
+    cap<-paste0("\n  \nFigure &&figno. Negative log-likelihood values for fits to effort in the ",fsh,".\n   \n")
+    plots[[cap]]<-p;
+        
     return(plots);
 }
