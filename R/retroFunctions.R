dae5922b8f51868e1332261e483ecfe1470098b8
William Stockhausen <williamstockhausen@MacBook-Pro.local>
2020-08-27 17:41:20 -0700

    Added functions to facilitate analysis of retrospective model runs. Incremented version to 2020.08.27.

---
+++ R/retroFunctions.R
@@ -0,0 +1,79 @@
+#'
+#'@title Get a melted dataframe of model estimates from a list of retrospective model results
+#'
+#'@description This function extracts a melted dataframe from a list of retrospective model results.
+#'
+#'@param modRetro - named list of resLst objects, with names corresponding to retrospective peels
+#'@param FUN - rCompTCMs function used to extract the desired estimates
+#'@param yadj - adjustment to make to model years
+#'
+#'@return dataframe
+#'
+#'@details Results are extracted using \code{FUN} for a retrospective series of tcsam02 model runs.
+#'
+#'@export
+#'
+retroGetMDFR<-function(modRetro,
+                       FUN,
+                       yadj=0){
+  dfrp<-FUN(modRetro);
+  dfrp$y<-dfrp$y+yadj;
+  dfrp<-dfrp[dfrp$y<=(as.numeric(as.character(dfrp$case))),];
+  return(dfrp);
+}
+
+#'
+#'@title Calculate Mohn's rho from a dataframe of retrospective estimates
+#'
+#'@description This function calculate Mohn's rho from a dataframe of retrospective estimates,
+#'and optionally produces a plot.
+#'
+#'@param dfr - dataframe of retrospective estimates for analysis
+#'@param ylab - y axis label for plot
+#'@param showPlot - flag (T/F) to show plot immmediately
+#'
+#'@return Returns a list with rho, a dataframe of values, and a ggplot2 object, invisibly
+#'
+#'@details Mohn's rho quantifies the relative bias exhibited by estimates of some
+#'quantity from a set of retrospective model runs.
+#'
+#'@import ggplot2
+#'@import magrittr
+#'@importFrom reshape2 dcast
+#'
+#'@export
+#'
+retroCalcMohnsRho<-function(dfr,ylab="value",showPlot=FALSE){
+  dfr$case<-as.character(dfr$case);
+  peels<-as.character(sort(unique(as.numeric(dfr$case)),decreasing=TRUE));
+  np<-length(peels);
+  bsyrc<-peels[1];
+  bsyrn<-as.numeric(bsyrc);
+  dfr<-dfr %>% subset(y>=as.numeric(peels[np]));
+  dfrp<-reshape2::dcast(dfr,y~case,value.var="val",fun.aggregate=sum,na.rm=FALSE);
+  dfrp<-dfrp %>% subset(y>(bsyrn-np));
+  mat<-as.matrix(dfrp[,2:ncol(dfrp)]);
+  mat<-mat[,peels];#columns now starts with base year
+  rownames(mat)<-as.character(dfrp$y);
+
+  dfrRB<-NULL;
+  for (p in 2:np){
+    peel<-peels[p];
+    base<-mat[peel,bsyrc];
+    retro<-mat[peel,peel];
+    rb<-(retro-base)/base;
+    dfrRB<-rbind(dfrRB,
+                 data.frame(peel=peel,base=base,retro=retro,relbias=rb));
+  }
+  rho<-mean(dfrRB$relbias);
+  trho<-prettyNum(rho,digits=3);
+
+  p<-ggplot(data=dfr,mapping=aes_string(x="y",y="val",colour="case"));
+  p<-p+geom_line();
+  p<-p+geom_point(data=dfr %>% subset(y==as.numeric(case)),size=2);
+  p<-p+labs(x="peel",y=ylab,colour="peel",caption=paste0("Mohn's rho = ",trho));
+  p<-p+lims(y=c(0,NA));
+  if (showPlot) print(p);
+
+  return(invisible(list(rho=rho,dfr=dfrRB,plot=p)));
+}
---
+++ R/retroPlots.R
@@ -0,0 +1,177 @@
+#'
+#'@title Create plots of several OFL-related quantities from a retrospective analysis
+#'
+#'@description This function create plots for several OFL-related quantities from a retrospective analysis.
+#'
+#'@param dfr.OFLs - dataframe of OFL-related values from a retrospective analysis
+#'@param x - name of dataframe column with "peel"labels
+#'@param xlab - label for x-axis
+#'
+#'@return list of ggplot2 objects
+#'
+#'@details Creates bar plots for average recruitment, Bmsy, Fmsy, terminal year MMB,
+#'projected MMB, and status
+#'
+#'@import ggplot2
+#'@import magrittr
+#'@importFrom reshape2 dcast
+#'
+#'@export
+#'
+retroPlotOFLResults<-function(dfr.OFLs,x="case",xlab="Peel"){
+  plots<-list();
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="avgRec",colour="case",fill="case",
+                     xlab=xlab,ylab="Average Recruitment (millions)");
+  plots[["Average Recruitment (millions)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="Bmsy",colour="case",fill="case",
+                     xlab=xlab,ylab="Bmsy (1000's t)");
+  plots[["Bmsy (1000's t)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="Fmsy",colour="case",fill="case",
+                     xlab=xlab,ylab="Fmsy (per year)");
+  plots[["Fmsy (per year)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="curB",colour="case",fill="case",
+                     xlab=xlab,ylab="Terminal MMB (1000's t)");
+  plots[["Terminal MMB (1000's t)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="prjB",colour="case",fill="case",
+                     xlab=xlab,ylab="Projected MMB (1000's t)");
+  plots[["Projected MMB (1000's t)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="status",colour="case",fill="case",
+                     xlab=xlab,ylab="status (projected MMB/Bmsy)");
+  p <- p + ggplot2::geom_hline(yintercept=1.0,linetype=2,colour="dark blue")
+  p <- p + ggplot2::geom_hline(yintercept=0.5,linetype=3,colour="red")
+  plots[["status (projected MMB/Bmsy)"]]<-p;
+
+  p <- plotMDFR.Bars(dfr.OFLs,x=x,value.var="OFL",colour="case",fill="case",
+                     xlab=xlab,ylab="OFL (1,000's t)");
+  plots[["OFL (1,000's t)"]]<-p;
+
+  return(plots)
+}
+
+
+#'
+#'@title Create a plot of time series from a retrospective analysis
+#'
+#'@description This function creates a plot of time series from a retrospective analysis.
+#'
+#'@param dfr - dataframe of estimated values extracted from a series of retrospective model runs
+#'@param ylab - label for y-axis
+#'@param units - label for y-axis units
+#'@param lnscale - flag to use log scale
+#'@param dodge - amount to dodge overlapping values
+#'@param size - point shape size
+#'@param colour - column name indicating retrospective model (default='case')
+#'@param shape - column name for shapes (default=value assigned to colour)
+#'@param verbose - flag
+#'
+#'@return a ggplot2 object
+#'
+#'@details Uses \code{rCompTCMs::plotMDFR.XY} to plot the timeseries.
+#'
+#'@export
+#'
+retroPlotTimeSeries<-function(dfr,ylab,units,lnscale=FALSE,dodge=0,size=2,
+                              colour="case",shape=colour,verbose=FALSE){
+  if (verbose) cat("colour = '",colour,"'\n","shape = '",shape,"'\n",sep="");
+  p<-rCompTCMs::plotMDFR.XY(dfr,
+                            x='y',value.var="val",agg.formula=NULL,
+                            xlab='year',ylab=ylab,units=units,lnscale=lnscale,
+                            dodge=dodge,size=size,
+                            colour=colour,guideTitleColour=ifelse(colour=="case",'peel',""),
+                            shape=shape,guideTitleShape=ifelse(shape=="case",'peel',""));
+  return(p)
+}
+
+#'
+#'@title Create a plot of model fits to time series from a retrospective analysis
+#'
+#'@description This function creates a plot of model fits to time series from a retrospective analysis.
+#'
+#'@param dfr - dataframe of estimated values extracted from a series of retrospective model runs
+#'@param ylab - label for y-axis
+#'@param units - label for y-axis units
+#'@param lnscale - flag to use log scale
+#'@param dodge - amount to dodge overlapping values
+#'@param size - point shape size
+#'@param colour - column name indicating retrospective model (default='case')
+#'@param shape - column name for shapes (default=value assigned to colour)
+#'@param facet_grid - formula to apply faceting
+#'@param verbose - flag
+#'
+#'@return a ggplot2 object
+#'
+#'@details Uses \code{rCompTCMs::plotMDFR.XY} to plot the timeseries.
+#'
+#'@export
+#'
+retroPlotFitsAsTimeSeries<-function(dfr,ylab,units,lnscale=FALSE,dodge=0,size=2,
+                                   colour="case",shape=colour,
+                                   facet_grid=termyear~.,
+                                   verbose=FALSE){
+  if (verbose) cat("colour = '",colour,"'\n","shape = '",shape,"'\n",sep="");
+  p<-rCompTCMs::plotMDFR.XY(dfr,
+                            x='y',value.var="val",agg.formula=NULL,facet_grid=facet_grid,
+                            xlab='year',ylab=ylab,units=units,lnscale=lnscale,
+                            dodge=dodge,size=size,
+                            colour=colour,guideTitleColour='',
+                            shape=shape,guideTitleShape='');
+  return(p)
+}
+
+#'
+#'@title Create a plot of values from a retrospective analysis as bars
+#'
+#'@description This function creates a plot of values from a retrospective analysis as bars.
+#'
+#'@param dfr - dataframe of estimated values extracted from a series of retrospective model runs
+#'@param xlab - label for x-axis
+#'@param ylab - label for y-axis
+#'@param facet_grid - formula to apply faceting
+#'
+#'@return a ggplot2 object
+#'
+#'@details The column "case" should indicate the peels of the retrospective runs
+#'
+#'@import ggplot2
+#'
+#'@export
+#'
+retroPlotValuesAsBars<-function(dfr,xlab="peel",ylab="",facet_grid=NULL){
+  p<-ggplot(data=dfr,mapping=aes_string(x="case",weight="val",fill="case"));
+  p<-p+geom_bar(colour=NA,position=position_dodge());
+  p<-p+labs(x=xlab,y=ylab,fill=xlab);
+  if (!is.null(facet_grid)) p<-p+facet_grid(facet_grid);
+  return(p)
+}
+
+#'
+#'@title Create a plot of values across size from a retrospective analysis
+#'
+#'@description This function creates a plot of values across size from a retrospective analysis.
+#'
+#'@param dfr - dataframe of estimated values across size extracted from a series of retrospective model runs
+#'@param xlab - label for x-axis
+#'@param ylab - label for y-axis
+#'@param facet_grid - formula to apply faceting
+#'
+#'@return a ggplot2 object
+#'
+#'@details The column "case" should indicate the peels of the retrospective runs
+#'
+#'@import ggplot2
+#'
+#'@export
+#'
+retroPlotValuesBySize<-function(dfr,xlab="size (mm CW)",ylab="",facet_grid=NULL){
+  p<-ggplot(data=dfr,mapping=aes_string(x="z",y="val",colour="case",shape="case"));
+  p<-p+geom_line()+geom_point();
+  p<-p+labs(x=xlab,y=ylab,colour="peel",shape="peel")
+  if (!is.null(facet_grid)) p<-p+facet_grid(facet_grid);
+  return(p)
+}
+
